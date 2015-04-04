d0451119b488ff6b6efb349d2dea3b37c6daa9bc
dgrahn <github@grahn.us>
2015-04-04 12:47:24 -0400

    Add emotion constants.

--- server/app/models/emotion.rb
+++ server/app/models/emotion.rb
@@ -1,2 +1,13 @@
 class Emotion < ActiveRecord::Base
+
+  # List: http://apps.timwhitlock.info/emoji/tables/unicode
+  GRIN  = ['1F601'.hex].pack("U*") # Grinning face with smiling eyes
+  SMILE = ['1F603'.hex].pack("U*") # Smiling face with open mouth
+  BEAM  = ['1F606'.hex].pack("U*") # Smiling face with open mouth and tightly-closed eyes
+  SMIRK = ['1F60F'.hex].pack("U*") # Smirking face
+  FROWN = ['1F61E'.hex].pack("U*") # Dissappointed face
+
+  belongs_to :post
+  belongs_to :user
+
 end
--- server/db/schema.rb
+++ server/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150404153045) do
+ActiveRecord::Schema.define(version: 20150404162057) do
+
+  create_table "emotions", force: true do |t|
+    t.string   "emotion"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: true do |t|
     t.string   "text"
